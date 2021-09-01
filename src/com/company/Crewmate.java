79f3c18b921bb07e8101626ad278d9a1a26d4ee5
MorganL-R <morganlawleyrobin@hotmail.com>
2021-09-02 00:18:54 +0100

    Added Crewmate class

---
+++ src/com/company/Crewmate.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Crewmate {
+    private String colour;
+    private int id;
+    private boolean isSus;
+    private boolean isDead;
+
+
+    public Crewmate(String colour, int id, boolean isSus, boolean isDead) {
+        this.colour = colour;
+        this.id = id;
+        this.isSus = isSus;
+        this.isDead = isDead;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isSus() {
+        return isSus;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public static Crewmate createCrewmate(String colour, int id, boolean isSus, boolean isDead){return new Crewmate(colour, id, isSus, isDead); }
+
+}
